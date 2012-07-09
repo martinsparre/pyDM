ac4b43f9db0029b064bbd186accae8a93363ec87
Martin Sparre <martinsparre@gmail.com>
2012-07-09 09:33:21 +0200

    Calculation of Virial theorem ratio.

--- Examples/GetSnapshot.py
+++ Examples/GetSnapshot.py
@@ -4,8 +4,17 @@ import Classes.DM_structure as DM_structure
 import scipy
 
 
-from mayavi import mlab
-
-FileName='Hq0.001.bin'
-A = DM_structure.DM_structure(FileName, ImportPotential=False)
-
+FileName='Hq1e6_000'
+A = DM_structure.DM_structure(FileName)
+S = A.Snapshot
+R = scipy.sqrt( S.x**2 + S.y**2 + S.z**2 )
+Beta = 0.1
+S.vx *= scipy.sqrt(Beta)
+S.vy *= scipy.sqrt(Beta)
+S.vz *= scipy.sqrt(Beta)
+T = scipy.sum(0.5*S.m*(S.vx**2+S.vy**2+S.vz**2))
+U = 0.5*scipy.sum(S.m*S.V)
+print 2*T/scipy.fabs(U)
+#plt.semilogx()
+#plt.plot(R,S.V)
+#plt.show()
